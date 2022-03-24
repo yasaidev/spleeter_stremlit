2268adff6ceeb9212639cc8847fc85862989df6d
ryo <3955027+older4@users.noreply.github.com>
2022-03-24 22:14:14 +0900

    add: 👏single audio file mode done!

--- spleeter_stremlit/main.py
+++ spleeter_stremlit/main.py
@@ -1,13 +1,13 @@
 import os
 from pathlib import Path
-from turtle import onclick
-from typing import List
+from typing import Generator, List
 
 import streamlit as st
 from spleeter.separator import Codec
 
-from utils import (ProcessingMode, SpleeterMode, download_youtube_as_mp3,
-                   split_audio)
+from utils import (ProcessingMode, SpleeterMode, SpleeterSettings,
+                   download_youtube_as_mp3, get_audio_separated_zip,
+                   get_split_audio)
 
 # global variables
 UPLOAD_DIR = Path("./upload_files/")
@@ -20,40 +20,43 @@ if 'audio_files' not in st.session_state:
     st.session_state.audio_files = []
 if 'output_files' not in st.session_state:
     st.session_state.output_files = []
-if 'selected_audio' not in st.session_state:
-    st.session_state.selected_audio = None
-
-audio_files: List[Path] = st.session_state.audio_files
+if 'spleeter_settings' not in st.session_state:
+    st.session_state.spleeter_settings = None
+if 'selected_music_file' not in st.session_state:
+    st.session_state.selected_music_file = None
 # states updater -------------------------------------------------------------
 
 
 def add_audio_files(audio_file: Path):
-    if(audio_file not in audio_files):
-        st.session_state.audio_files = audio_files + [audio_file]
+    if(audio_file not in st.session_state.audio_files):
+        st.session_state.audio_files.append(audio_file)
 
 
 def save_uploaded_file(upload_file) -> Path:
-    # check if already uploaded
-    # if(os.path.exists(UPLOAD_DIR / upload_file.name)):
-    #     st.warning("This file is already in the list")
     os.makedirs(UPLOAD_DIR, exist_ok=True)
-    file_path = UPLOAD_DIR / upload_file.name
+
+    escaped_file_path = Path(upload_file.name)
+
+    # check if upload_file name last charactor is not space charactor
+    if(escaped_file_path.stem[-1] == " "):
+        escaped_file_path = escaped_file_path.parent / \
+            f"{escaped_file_path.stem[:-1]}{escaped_file_path.suffix}"
+
+    file_path = UPLOAD_DIR / escaped_file_path
+    print(f"upload file:{file_path}")
     with open(file_path, 'wb') as f:
         f.write(upload_file.read())
+
     return file_path
-
-
-def show_audio(audio_file: Path):
-    print(audio_file)
 
 
 # sidebar start --------------------------------------------------------------
 st.sidebar.write("""
-## Audio upload
+# Audio upload
 """)
 
 st.sidebar.write("""
-### from local audio file
+# From local audio file
 """)
 
 # audio file uploader
@@ -68,7 +71,7 @@ for audio_file in upload_files:
 # youtubedl mp3
 # text input area for youtube url
 st.sidebar.write("""
-### from YouTube url
+# From YouTube url
 """)
 
 
@@ -77,13 +80,11 @@ def youtube_dl_wrapper(url):
         st.warning("Please enter a valid url")
     else:
         st.info("Downloading...")
-        is_exist, file_name = download_youtube_as_mp3(url, UPLOAD_DIR)
+        file_name, is_exist = download_youtube_as_mp3(url, UPLOAD_DIR)
         file_path = Path(file_name)
         # check if already uploaded in audio_file
-        if(len(filter(lambda x: x.absolute().name == file_path.absolute().name) != 0)):
-            st.warning("This file is already in the list")
-        elif(is_exist):
-            st.info("File already downloaded")
+        if(is_exist):
+            st.warning("File already downloaded")
             add_audio_files(file_name)
         else:
             st.success("File downloaded successfully")
@@ -91,14 +92,19 @@ def youtube_dl_wrapper(url):
 
 
 with st.sidebar.form("youtube_dl_form"):
-    quality_val = st.slider("Bitrate", 1, 512, 192,
-                            )
     youtube_url = st.text_input("Enter a youtube url", "")
-
+    quality_val = st.slider("Bitrate", 1, 512, 192)
     # Every form must have a submit button.
     submitted = st.form_submit_button("Download")
     if submitted:
         youtube_dl_wrapper(youtube_url)
+
+st.sidebar.write("# Reload already uploaded audio files")
+if st.sidebar.button("Reload"):
+    st.sidebar.info("Reloading...")
+    for file in Path(UPLOAD_DIR).glob("*"):
+        add_audio_files(file)
+    st.sidebar.success("Done!")
 
 # sidebar end --------------------------------------------------------------
 
@@ -112,42 +118,92 @@ current_mode = st.selectbox(
 
 if(current_mode == ProcessingMode.SINGLE):
 
-    st.write("""
-    #### Mode: Split single audio file
-    """)
-
-    selected_music = None
-    select_stems = None
-    select_codec = None
-    select_bitrate = None
+    selected_music: Path
+    select_stems: SpleeterMode
+    select_codec: Codec
+    select_bitrate: int
+    output_files_generator: Generator[Path, None, None]
 
     with st.form("spleenter"):
+        st.subheader("Mode: "+current_mode.value)
         selected_music = st.selectbox(
-            "select a audio file", st.session_state.audio_files, format_func=lambda x: x.name, key="selected_audio")
+            "Select an audio file", st.session_state.audio_files, help="To select audio, you have to upload or download an audio file at least once",
+            format_func=lambda x: x.name)
 
         select_stems = st.selectbox(
             "Selected split mode", SpleeterMode, format_func=lambda x: x.value.label)
 
-        col1, col2 = st.columns(2)
-        with col1:
-            select_codec = st.selectbox(
-                "Codec", list(Codec), format_func=lambda x: x.value, index=1)
-        with col2:
-            select_bitrate = st.slider(
-                "Bitrate", 1, 512, 192)
+        with st.expander("Detail Settings"):
+            col1, col2 = st.columns(2)
+            with col1:
+                st.subheader("Output audio settings")
+                select_codec = st.selectbox(
+                    "Codec", list(Codec), format_func=lambda x: x.value, index=1)
+                select_bitrate = st.slider(
+                    "Bitrate", 1, 512, 192)
+            with col2:
+                st.subheader("Spleetor processing settings")
+                use_mwf: bool = st.checkbox(
+                    "Use multi-channel Wiener filtering", value=True, help="Use multi-channel Wiener filtering to improve the quality of the output audio, but this may increase the processing time")
+                use_16kHz: bool = st.checkbox(
+                    "Use 16kHz model (instead of 11kHz)", value=True, help="Use 16kHz model is better for high quality audio than 11kHz, but it may increase the processing time")
+                duaration_minutes: int = st.slider(
+                    "Max duration minutes", 0, 60, 10, help="Max duration minutes of the audio to be processed. If the audio is longer than the duration, the audio will be ignored.")
 
         if st.form_submit_button("Split"):
-            st.session_state.output_files = split_audio(
-                str(selected_music), select_stems,
-                select_codec, select_bitrate, OUTPUT_DIR)
+            # check if settings are selected:
+            if(selected_music == None or select_stems == None):
+                st.error("Please select an audio file.")
+
+            else:
+                current_settings = SpleeterSettings(
+                    select_stems,
+                    select_codec,
+                    select_bitrate,
+                    use_mwf,
+                    use_16kHz,
+                    duaration_minutes*60
+                )
+                st.session_state.spleeter_settings = current_settings
+                st.session_state.selected_music_file = selected_music
+                st.session_state.output_files = []
+                with st.spinner('Wait for spleeter processing...'):
+                    output_files_generator, is_exist = get_split_audio(
+                        current_settings,
+                        selected_music,
+                        OUTPUT_DIR)
+                    for x in output_files_generator:
+                        st.session_state.output_files.append(x)
+                st.success("Done!")
 
     with st.container():
-        st.write("#### Output")
-        if(st.session_state.selected_audio != None and select_stems != None):
-            col1, col2 = st.columns(2)
-            col1.metric("Audio", st.session_state.selected_audio.name)
-            col2.metric("Mode", select_stems.value.name)
+        st.subheader("Output")
+        if(st.session_state.selected_music_file != None and select_stems != None):
+            col1, col2, col3 = st.columns(3)
+            with col1:
+                st.caption("Audio:")
+                st.write(st.session_state.selected_music_file.name)
+            with col2:
+                st.caption("Mode:")
+                st.write(
+                    f"{select_stems.value.name}{'-16kHz' if use_16kHz else '-11kHz'}{'-mwf' if use_mwf else '-no-mwf'}")
+            with col3:
+                st.caption("Zip:")
+                zip_file_path = get_audio_separated_zip(
+                    audio_file=st.session_state.selected_music_file,
+                    output_path=OUTPUT_DIR,
+                    config=st.session_state.spleeter_settings,
+                )
+                with open(zip_file_path, 'rb') as f:
+                    st.download_button(
+                        label="Download",
+                        data=f,
+                        file_name=zip_file_path.name,
+                    )
+            st.caption("Original audio: " +
+                       st.session_state.selected_music_file.name)
+            st.audio(str(selected_music))
 
-        for i, audio_file in enumerate(st.session_state.output_files):
-            st.caption(audio_file.name)
-            st.audio(str(audio_file))
+            for i, audio_file in enumerate(st.session_state.output_files):
+                st.caption(audio_file.name)
+                st.audio(str(audio_file))
--- spleeter_stremlit/utils.py
+++ spleeter_stremlit/utils.py
@@ -1,8 +1,10 @@
 import os
+import shutil
 from dataclasses import dataclass
 from enum import Enum
+from importlib.resources import path
 from pathlib import Path
-from typing import Tuple
+from typing import Generator, List, Tuple
 
 import youtube_dl
 from spleeter.separator import Codec, Separator
@@ -11,8 +13,8 @@ from spleeter.separator import Codec, Separator
 class ProcessingMode(Enum):
     # mode enum
     SINGLE = "Split a single audio file"
-    MULTIPLE = "Split multiple audio files"
-    COMBINE = "Combine splited audio"
+    MULTIPLE = "Split multiple audio files at once"
+    # COMBINE = "Combine splited audio"
 
 
 @dataclass
@@ -41,19 +43,46 @@ class SpleeterMode(Enum):
     )
 
 
-def download_youtube_as_mp3(youtube_url: str, output_path: str) -> Tuple[bool, str]:
-    # get audio file name from youtube url by using youtube-dl
+@dataclass
+class SpleeterSettings:
+    """
+    Settings for spleeter
+    Attributes:
+        split_mode (SpleeterMode): Spleeter mode
+        codec (Codec): Codec to use
+        bitrate (int): Bitrate to use
+        usemwf (bool): Use multi-channel Wiener filtering (default: False)
+        use16kHZ (bool): Use 16kHz sampling rate (default: False for 11kHz)
+        duration (int): Duration in seconds
+    """
+    split_mode: SpleeterMode
+    codec: Codec
+    bitrate: int
+    usemwf: bool = False
+    use16kHZ: bool = False
+    duration: int = 600
+
+
+def download_youtube_as_mp3(youtube_url: str, output_path: Path) -> Tuple[Path, bool]:
+    """
+    Download youtube video as mp3
+    Args:
+        youtube_url: str: youtube url
+        output_path: Path: output path
+    Returns:
+        Tuple[bool, str]: (is_exist, output_file_path)
+    """
     youtube_title = youtube_dl.YoutubeDL({}).extract_info(
         youtube_url, download=False)["title"]
 
     is_exist = True
 
     # check if audio file already exists
-    if not os.path.exists(output_path + youtube_title + ".mp3"):
+    if not os.path.exists(output_path / f"{youtube_title}.mp3"):
         is_exist = False
         ydl_opts = {
             'format': 'bestaudio/best',
-            'outtmpl':  output_path + "%(title)s.%(ext)s",
+            'outtmpl':  str(output_path.absolute()) + "/%(title)s.%(ext)s",
             'postprocessors': [
                 {'key': 'FFmpegExtractAudio',
                  'preferredcodec': 'mp3',
@@ -64,26 +93,82 @@ def download_youtube_as_mp3(youtube_url: str, output_path: str) -> Tuple[bool, s
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([youtube_url])
 
-    return (is_exist, output_path + youtube_title + '.mp3')
+    return output_path / f"{youtube_title}.mp3", is_exist
 
 
-def split_audio(audio_file: str, split_mode: SpleeterMode,
-                codec: Codec, bitrate: int, output_path: Path, mwf: bool = False):
-    print((output_path /
-          f'{Path(audio_file).stem}/vocals.{codec.value}').absolute())
+def get_split_audio(config: SpleeterSettings,
+                    audio_file: Path,
+                    output_path: Path) -> Tuple[Generator[Path, None, None], bool]:
+    """
+    Split audio file
+    Args:
+        SpleeterSettings: SpleeterSettings: spleeter settings
+    Returns:
+        Tuple[Generator[Path, None, None], bool]: (output_files, is_exist)
+    """
+
+    is_exist = False
+    output_path_base = Path(
+        output_path/f"{audio_file.stem}/{config.split_mode.value.name}{'-16kHz' if config.use16kHZ else '-11kHz'}{'' if config.usemwf else '-noMWF'}/")
+    print("output_path_base:" + str(output_path_base))
+
     # check if audio file exists
-    if os.path.exists(output_path/f'{Path(audio_file).stem}/vocals.{codec.value}'):
-        pass
+    if os.path.exists(output_path_base/f"{audio_file.stem}_vocals.{config.codec.value}"):
+        print(
+            f"{audio_file.stem} [{config.split_mode.value.name}{'-16kHz' if config.use16kHZ else ''}] : already splited")
+        is_exist = True
 
     else:
+        # create output path
+        os.makedirs(str(output_path_base.absolute()), exist_ok=True)
+
         # split audio file by using spleeter
         separator = Separator(
-            params_descriptor=f'spleeter:{split_mode.value.name}',
-            MWF=mwf, multiprocess=False
-
+            params_descriptor=f"spleeter:{config.split_mode.value.name}{'-16kHz' if config.use16kHZ else ''}",
+            MWF=config.usemwf, multiprocess=False
         )
         separator.separate_to_file(
-            audio_file, bitrate=bitrate, destination=output_path, codec=codec,)
+            str(audio_file),
+            bitrate=f"{config.bitrate}k",
+            destination=str(output_path),
+            codec=config.codec,
+            filename_format=f"{{filename}}/{config.split_mode.value.name}{'-16kHz' if config.use16kHZ else '-11kHz'}{'' if config.usemwf else '-noMWF'}/{{filename}}_{{instrument}}.{{codec}}",
+            duration=config.duration
+        )
 
     # return output file path list
-    return Path(output_path/f'{Path(audio_file).stem}/').glob(f'*.{codec.value}')
+    return output_path_base.glob(f'*.{config.codec.value}'), is_exist
+
+
+def get_audio_separated_zip(config: SpleeterSettings,
+                            audio_file: Path,
+                            output_path: Path) -> Path:
+    """
+    Get separated audio zip file
+    Args:
+        audio_file: Path: audio file path
+        output_path: Path: audio file path
+        config: SpleeterSettings: spleeter settings
+    Returns:
+        Path: separated audio zip file path (EX: output_path/RYDEEN/RYDEEN_4stem.zip)
+    """
+    zip_file_basepath = Path(
+        output_path/f"{audio_file.stem}/{audio_file.stem}_{config.split_mode.value.name}{'-16kHz' if config.use16kHZ else ''}")
+    zip_file_path = zip_file_basepath.parent / \
+        Path(zip_file_basepath.name + ".zip")
+
+    # check if separated audio zip file already exists
+    if os.path.exists(zip_file_path):
+        print(
+            f"{audio_file.stem} [{config.split_mode.value.name}] : already zipped")
+
+    # get split audio path list and zip them
+    else:
+        separated_audio_path_list, is_exist = get_split_audio(
+            config, audio_file, output_path)
+        separated_audio_path_parent: Path = separated_audio_path_list.__next__().parent
+        # zip separated audio folder
+        shutil.make_archive(
+            str(zip_file_basepath), 'zip', str(separated_audio_path_parent))
+
+    return zip_file_path
